250434d8405768a7cd04c74daf35f558df98b873
Spratch <contact@josephclenet.fr>
2023-11-05 20:45:12 +0100

    modal btn name fix

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -2,15 +2,20 @@
 const body = document.getElementById("body");
 const modal = document.getElementById("contact_modal");
 const main = document.getElementById("main");
-const modalCloseBtn = document.getElementById("contact_button");
-const openCloseBtn = document.getElementById("openCloseBtn");
+const modalCloseBtn = document.getElementById("modalCloseBtn");
+const modalOpenBtn = document.getElementById("modalOpenBtn");
+const contactForm = document.getElementById("contact_form");
+const first = document.getElementById("first-name");
+const last = document.getElementById("last-name");
+const email = document.getElementById("email");
+const message = document.getElementById("message");
 
 // Open modal
 function displayModal() {
     main.setAttribute("aria-hidden", "true"); // Hide main content for Assistive Technologies
     modal.setAttribute("aria-hidden", "false"); // Show modal for AT
     body.style.overflow = "hidden"; // Prevent scrolling
-	modal.style.display = "flex"; // Show modal
+    modal.style.display = "flex"; // Show modal
     modalCloseBtn.focus(); // Focus on close button
 }
 
@@ -20,7 +25,7 @@ function closeModal() {
     modal.setAttribute("aria-hidden", "true"); // Show modal for AT
     body.style.overflow = "auto"; // Prevent scrolling
 	modal.style.display = "none"; // Show modal
-    openCloseBtn.focus(); // Focus on close button
+    modalOpenBtn.focus(); // Focus on close button
 }
 
 // Press Escape to close modal
@@ -34,6 +39,21 @@ document.addEventListener('keydown', e => {
     }
   });
 
-// function submitForm() {
-//     console.log();
-// }
+function submitForm() {
+    // Color invalid inputs
+    contactForm.classList.add("submitted");
+
+    // Submit datas
+    let formDatas = {
+        firstName: first.value,
+        lastName: last.value,
+        email: email.value,
+        message: message.value
+    }
+    console.log(formDatas);
+}
+
+contactForm.addEventListener("submit", (event) => {
+    // prevent default
+    event.preventDefault();
+});
