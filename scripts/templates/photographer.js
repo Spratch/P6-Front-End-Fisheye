8145de2409518b2142cb73deddec54efc5ccf510
Spratch <contact@josephclenet.fr>
2023-11-27 21:58:05 +0100

    photographer template refactoring

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,159 +1,121 @@
+// DOM elements creation with type of tag, table of attributes, and textContent
+function createDOMElement(tag, attributes = {}, textContent) {
+    // Tag
+    const element = document.createElement(tag);
+
+    // Attributes
+    for (const [key, value] of Object.entries(attributes)) {
+        element.setAttribute(key, value);
+    }
+
+    // Text
+    if (textContent) {
+        element.textContent = textContent;
+    }
+
+    return element;
+}
+
 function photographerTemplate(data) {
     const { name, portrait, id, city, country, tagline, price, title, image, video, likes } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
-    // DOM de la carte photographe
+    // Homepage photographers cards DOM
     function getUserCardDOM() {
-        // Creation de l'article
-        const article = document.createElement( 'article' );
-        article.setAttribute('id', `photographer-${id}`);
-        // Lien
-        const link = document.createElement( 'a' );
-        link.setAttribute('id', `link-${id}`);
-        link.setAttribute('href', `./photographer.html?id=${id}`);
-        link.setAttribute('aria-label', `${name}`);
-        // Image
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        img.setAttribute('alt', `photo of ${name}`)
-        // Nom
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        // Lieu
-        const location = document.createElement( 'p' );
-        location.textContent = `${city}, ${country}`;
-        location.classList.add('photographer-location');
-        // Tagline
-        const taglineElement = document.createElement( 'p' );
-        taglineElement.textContent = tagline;
-        taglineElement.classList.add('photographer-tagline');
-        // Prix
-        const pricing = document.createElement( 'p' );
-        pricing.textContent = `${price}€/jour`;
-        pricing.classList.add('photographer-pricing');
+        // DOM Elements creation
+        const article = createDOMElement('article', { id: `photographer-${id}` });
+        const link = createDOMElement('a', { id: `link-${id}`, href: `./photographer.html?id=${id}`, 'aria-label': `${name}` });
+        const img = createDOMElement('img', { src: picture, alt: `photo of ${name}` });
+        const h2 = createDOMElement('h2', {}, name);
+        const location = createDOMElement('p', { class: 'photographer-location' }, `${city}, ${country}`);
+        const taglineElement = createDOMElement('p', { class: 'photographer-tagline' }, tagline);
+        const pricing = createDOMElement('p', { class: 'photographer-pricing' }, `${price}€/jour`);
 
-        // DOM
-        article.appendChild(link);
-        link.appendChild(img);
-        link.appendChild(h2);
-        article.appendChild(location);
-        article.appendChild(taglineElement);
-        article.appendChild(pricing);
+        // DOM Generation
+        article.append(link, location, taglineElement, pricing);
+        link.append(img, h2);
 
         return (article);
     }
 
-    // DOM infos profil
+    // Photographer page infos DOM
     function getProfileDOM() {
-        const profile = document.createElement('div')
-        profile.classList.add('photographer-profile');
-        // H1
-        const h1 = document.createElement('h1');
-        h1.textContent = name;
-        // Lieu
-        const location = document.createElement( 'p' );
-        location.textContent = `${city}, ${country}`;
-        location.classList.add('photographer-location');
-        // Tagline
-        const taglineElement = document.createElement( 'p' );
-        taglineElement.textContent = tagline;
-        taglineElement.classList.add('photographer-tagline');
+        // DOM Elements creation
+        const profile = createDOMElement('div', { class: 'photographer-profile' });
+        const h1 = createDOMElement('h1', {}, name);
+        const location = createDOMElement('p', { class: 'photographer-location' }, `${city}, ${country}`);
+        const taglineElement = createDOMElement('p', { class: 'photographer-tagline' }, tagline);
+
+        // DOM Generation
+        profile.append(h1, location, taglineElement);
         
-        // DOM
-        profile.appendChild(h1);
-        profile.appendChild(location);
-        profile.appendChild(taglineElement);
         return (profile);
     }
 
-    // Profile picture
+    // Photographer page picture DOM
     function getProfilePic() {
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture);
-        img.setAttribute('alt', name);
-        img.classList.add('profile-picture')
+        const img = createDOMElement('img', { src: picture, alt: name, class: 'profile-picture' });
         return (img);
     }
 
+    // Photographer page media generation function
     function getMediaDOM(image, fromLightbox){
+        // Handle differently media if image or video
         const file = !image ? video : image;
         const element = !image ? 'video' : 'img';
         const attribut = !image ? 'muted' : 'alt';
         const value = !image ? 'true' : title;
 
-        const mediaElement = document.createElement(element);
-        mediaElement.setAttribute('src', `assets/images/${file}`);
-        mediaElement.setAttribute(attribut, value);
+        // DOM Generation
+        const mediaElement = createDOMElement(element, { src: `assets/images/${file}`, [attribut]: value });
         if (!image && fromLightbox) {
             mediaElement.setAttribute('controls', 'true');
             mediaElement.setAttribute('autoplay', 'true');
         }
+
         return mediaElement;
     }
     
-    // Portfolio content
+    // Photographer page media gallery DOM
     function getPortfolioDOM() {
-        const itemArticle = document.createElement('article');
-        itemArticle.classList.add('media-article');
-        itemArticle.setAttribute('aria-label', 'Vue détaillée');
-        // Media
+        // DOM Elements creation
+        const itemArticle = createDOMElement('article', { class: 'media-article', 'aria-label': 'Vue détaillée' });
         const mediaElement = getMediaDOM(image);        
-        // Infos
-        const infos = document.createElement("div");
-        infos.classList.add("portfolio-item__infos");
-        // Image title
-        const titleElement = document.createElement("p");
-        titleElement.textContent = title;
-        // Likes
-        const likesElement = document.createElement("p");
-        likesElement.classList.add("portofolio-item__likes");
-        likesElement.textContent = likes;
-        const likesHeart = document.createElement("i");
-        likesHeart.classList.add("fa-solid", "fa-heart");
-        likesHeart.setAttribute('style', 'color: #901C1C');
+        const infos = createDOMElement('div', { class: 'portfolio-item__infos' });
+        const titleElement = createDOMElement('p', {}, title);
+        const likesNumber = createDOMElement('p', { class: 'portofolio-item__likes' }, likes);
+        const likesHeart = createDOMElement('i', { class: 'fa-solid fa-heart', style: 'color: #901C1C' });
 
-        // DOM
-        itemArticle.appendChild(mediaElement);
-        itemArticle.appendChild(infos);
-        infos.appendChild(titleElement);
-        infos.appendChild(likesElement);
-        likesElement.appendChild(likesHeart);
+        // DOM Generation
+        itemArticle.append(mediaElement, infos);
+        infos.append(titleElement, likesNumber);
+        likesNumber.append(likesHeart);
+
         return itemArticle;
     }
 
     function getValueDOM(sumLikes) {
-        const valueDiv = document.createElement('div');
-        valueDiv.classList.add("value-container");
-        // Likes
-        const likesElement = document.createElement("p");
-        likesElement.classList.add("value-likes");
-        likesElement.textContent = sumLikes;
-        const likesHeart = document.createElement("i");
-        likesHeart.classList.add("fa-solid", "fa-heart");
-        likesHeart.setAttribute('style', 'color: black');
-        // Price
-        const dailyPrice = document.createElement('p');
-        dailyPrice.textContent = `${price}€ / jour`;
+        // DOM Elements creation
+        const valueDiv = createDOMElement('div', { class: 'value-container' });
+        const likesNumber = createDOMElement('p', { class: 'value-likes' }, sumLikes);
+        const likesHeart = createDOMElement('i', { class: 'fa-solid fa-heart', style: 'color: black' });
+        const dailyPrice = createDOMElement('p', {}, `${price}€ / jour`);
 
-        // DOM
-        valueDiv.appendChild(likesElement);
-        likesElement.appendChild(likesHeart);
-        valueDiv.appendChild(dailyPrice);
+        // DOM Generation
+        valueDiv.append(likesNumber, dailyPrice);
+        likesNumber.appendChild(likesHeart);
+
         return valueDiv;
     }
 
     function getLightboxDOM(i, mediaElement) {
-        const mediaParent = document.createElement("div");
-        mediaParent.classList.add("lightbox-media-container");
-        
-        // Media Title
-        const mediaTitle = document.createElement("p");
-        mediaTitle.textContent = title;
+        // DOM Elements creation
+        const mediaParent = createDOMElement('div', { class: 'lightbox-media-container'});
+        const mediaTitle = createDOMElement('p', {}, title);
 
-        // DOM
-        mediaParent.appendChild(mediaElement);
-        mediaParent.appendChild(mediaTitle);
+        // DOM Generation
+        mediaParent.append(mediaElement, mediaTitle);
 
         return mediaParent;
     }
